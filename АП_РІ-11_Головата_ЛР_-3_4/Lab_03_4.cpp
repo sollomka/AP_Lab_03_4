9474bdb8b699b11ca8a60ff06c1ef1ea04514bd2
sollomka <solomiia.holovata.ri.2022@lpnu.ua>
2022-10-14 01:51:08 +0300

    first

---
+++ АП_РІ-11_Головата_ЛР_-3_4/Lab_03_4.cpp
@@ -0,0 +1,34 @@
+// Lab_03_4.cpp
+// Головатої Соломії
+// Лабораторна робота № 3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 5
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x, y, R;
+
+	cout << "x = "; cin >> x;
+	cout << "y = "; cin >> y;
+	cout << "R = "; cin >> R;
+
+	// розгалуження в повній формі
+	if (x >= -R && x <= 0 && y >= 0 && y <= R && x + R >= y)
+		cout << "yes" << endl;
+
+	else
+		if (x >= -sqrt(pow(R, 2) - pow(y, 2)) && x <= 0 && y >= -R && y <= 0)
+		cout << "yes" << endl;
+	else 
+		if (x >= 0 && x <= sqrt(pow(R, 2) - pow(y, 2)) && y >= 0 && y <= R)
+		cout << "yes" << endl;
+
+	else 		cout << "no" << endl;
+	system("pause");
+	return 0;
+}
